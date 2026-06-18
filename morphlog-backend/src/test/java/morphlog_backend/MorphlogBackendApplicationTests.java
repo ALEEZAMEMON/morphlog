6679bf0beb316fce60116fd46f02c5ba9f4726dc
Aleeza Memon <aleezaakbar659@gmail.com>
2026-06-18 23:20:43 +0500

    initial: added Spring Boot backend and React Native app with full folder structure

---
+++ morphlog-backend/src/test/java/morphlog_backend/MorphlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package morphlog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorphlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
